d796fadad1cc1f45934a3a63384da3a516eac28d
zngyng <yingz@YingdeMacBook-Pro.local>
2017-05-01 00:13:12 -0400

    change4

---
+++ mySketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0,0,0);
+  stroke(0,0,255);
+  noFill();
+
+
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 20, 20);
+
+
+
+
+
+}
